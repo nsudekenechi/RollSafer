167f671deca60f073f3bcc87dc969c53a34ada6d
Kenechi <nsudekenechi2@gmail.com>
2023-10-27 17:08:53 +0100

    Fixed Car Slides

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,3 +1,6 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.min.css";
 export const Header = () => {
   const images = [1, 2, 3, 4];
   return (
@@ -24,19 +27,32 @@ export const Header = () => {
 
         <div className="hidden md:block md:col-span-7 ">
           <div className="h-[400px] flex justify-center items-center">
-            <img src="./Images/Cars/1.png" alt="" className="h-[100%] w-[100%]"/>
+            <Swiper modules={[Navigation, Pagination]}
+              pagination={{
+                clickable: true,
+                el: ".con",
+                renderBullet: (index, className) => (
+                  `<span class='${className} w-fit h-fit bg-transparent' >
+            <img  src=./Images/CarLogos/${images[index]}.png  class="w-[50px] h-[50px] object-contain grayscale hover:grayscale-0 duration-700 hover:scale-150 scale-110 cursor-pointer"/>
+            </span> `
+                )
+              }}
+              loop={true} >
+              <SwiperSlide>
+                <img src="./Images/Cars/rolls.png" alt="" className="h-[100%] w-[100%]" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="./Images/Cars/1.png" alt="" className="h-[100%] w-[100%]" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="./Images/Cars/tesla.png" alt="" className="h-[100%] w-[100%]" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="./Images/Cars/benz.png" alt="" className="h-[100%] w-[100%]" />
+              </SwiperSlide>
+            </Swiper>
           </div>
-          <div className="flex gap-x-20 items-center justify-center ">
-            {images.map((image) => (
-              <div className="" key={image}>
-                <img
-                  src={`./Images/CarLogos/${image}.png`}
-                  alt=""
-                  className="w-[50px] h-[50px] object-contain grayscale hover:grayscale-0 duration-700 hover:scale-150 scale-110 cursor-pointer"
-                />
-              </div>
-            ))}
-          </div>
+          <div className="flex gap-x-20 items-center justify-center con"></div>
         </div>
       </div>
     </header>
